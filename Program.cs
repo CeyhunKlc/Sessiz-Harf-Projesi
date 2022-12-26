21ec9a55d99f214312f7388b5b01bebf2958bbb5
CeyhunKlc <117180182+CeyhunKlc@users.noreply.github.com>
2022-12-26 11:30:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace sessizHarf
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Aralarında Boşluk Bırakarak Kelime Giriniz:");
+            string[] kelimeler = Console.ReadLine().Split();
+
+            foreach (var item in kelimeler)
+            {
+                bool deger = false;
+                for (int i = 1; i < item.Length; i++)
+                {
+                    if (Sessiz(item[i]) == Sessiz(item[i - 1]))
+                        deger = true;
+                }
+                
+                Console.Write(deger + " ");
+            }
+
+            bool Sessiz(char c)
+            {
+                return !"aeıioöuü".ToCharArray().Contains(c);
+            }
+        }
+    }
+}
